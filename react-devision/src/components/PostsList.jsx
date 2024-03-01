4500213c12948cb89e1a20718f19a3b2b4803e5e
김현정(Hyunjung Kim) <withcusco@gmail.com>
2024-03-01 22:54:53 +0900

    [docs] newpost 수정

--- react-devision/src/components/PostsList.jsx
+++ react-devision/src/components/PostsList.jsx
@@ -1,14 +1,30 @@
 import Post from './Post';
 import styles from './PostsList.module.css';
 import NewPost from './NewPost';
+import { useState } from 'react';
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  // 상태를 변화시키는 함수
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        // 함수를 실행시키는게 아니라 속성의 "값으로" 함수를 넘긴다
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={styles.posts}>
-        <Post author="joy" body="react is awesome!" />
-        <Post author="march" body="check out our new website!" />
+        <Post author={enteredAuthor} body={enteredBody} />
       </ul>
     </>
   );
